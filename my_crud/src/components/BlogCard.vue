efb19b454943c04f79ab27bd7e30f6bc4c578ff7
Chalisa424 <>
2025-08-20 14:01:28 +0700

    build navbar , searchbar

---
+++ my_crud/src/components/BlogCard.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-white">
+        
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
---
+++ my_crud/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="bg-white shadow-md border-gray-200">
+    <div class="flex flex-wrap justify-between mx-auto px-10 py-5">
+      <span class="text-3xl">บทความ</span>
+      <div class="flex space-x-6">
+        <svg
+          class="w-6 h-6 text-gray-500 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 16 16"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 8h11m0 0-4-4m4 4-4 4m-5 3H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h3"
+          />
+        </svg>
+        
+        <a
+          href="#"
+          class="text-2xl text-gray-500 hover:underline hover:text-blue-500"
+          >Longout</a
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+<script setup lang="ts">
+</script>
+
